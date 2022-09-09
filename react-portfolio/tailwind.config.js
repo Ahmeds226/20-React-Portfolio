c63f696487a186b82721e3cc3b7c56c26b212f9e
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 05:38:42 +0100

    working on config files

--- react-portfolio/tailwind.config.js
+++ react-portfolio/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Configured so that I have added the paths to all of my template files:
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
