6da5b845365c0f691c7dabb322788458b7cc27af
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 06:39:01 +0100

    Added my first project

--- react-portfolio/src/data/projects.js
+++ react-portfolio/src/data/projects.js
@@ -1,3 +1,15 @@
-const projects = [];
+const projects = [
+  {
+    // Horiseon Code Refactor:
+    image:
+      "https://raw.githubusercontent.com/Ahmeds226/Week-1-Homework/master/images/01-html-css-git-homework-demo.png",
+    alt: "picture of Horiseon Code Refactor",
+    title: "Horiseon Code Refactor",
+    description:
+      "An improved codebase, for a marketing agency , that follows accessibility standards to omptimise their webisite for search engines. ",
+    repositoryLink: "https://github.com/Ahmeds226/Week-1-Homework",
+    deployedLink: "https://ahmeds226.github.io/Week-1-Homework/",
+  },
+];
 
 export default projects;
