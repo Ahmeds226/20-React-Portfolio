27e8ff8a50cc6bdb7b3a88ecc775b17dd3505e21
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 16:51:35 +0100

    Added navbar to app.js and function

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,3 +1,12 @@
 // Import all components and sections here!!!
+import NavbarComponent from "./components/NavbarComponent";
 
+function App() {
+  return (
+    <div>
+      <NavbarComponent />;
+    </div>
+  );
+}
 // Export here
+export default App;
