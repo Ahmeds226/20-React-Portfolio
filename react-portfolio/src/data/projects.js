959864cbcd890478b65088b2585ba7fc02a57bf9
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 13:34:13 +0100

    Added weather dashboard project

--- react-portfolio/src/data/projects.js
+++ react-portfolio/src/data/projects.js
@@ -8,8 +8,8 @@ const projects = [
     title: "Horiseon Code Refactor",
     description:
       "An improved codebase, for a marketing agency, that follows accessibility standards to optimise their website for search engines.",
-    repositoryLink: "https://github.com/Ahmeds226/Week-1-Homework",
     deployedLink: "https://ahmeds226.github.io/Week-1-Homework/",
+    repositoryLink: "https://github.com/Ahmeds226/Week-1-Homework",
   },
   {
     // Original Professional Portfolio:
@@ -18,8 +18,8 @@ const projects = [
     alt: "Preview of my original portfolio",
     title: "Original Portfolio",
     description: "My original portfolio.",
-    repositoryLink: "https://github.com/Ahmeds226/Portfolio-Project",
     deployedLink: "https://ahmeds226.github.io/Portfolio-Project/",
+    repositoryLink: "https://github.com/Ahmeds226/Portfolio-Project",
   },
   {
     // Secure Password Generator:
@@ -29,8 +29,8 @@ const projects = [
     title: "Secure Password Generator",
     description:
       "An application that enables employees, that may have sensitive data, to generate random passwords based on criteria that they have selected so that they have greater security.",
-    repositoryLink: "https://github.com/Ahmeds226/secure-password-generator",
     deployedLink: "https://ahmeds226.github.io/secure-password-generator/",
+    repositoryLink: "https://github.com/Ahmeds226/secure-password-generator",
   },
   {
     // Code Quiz:
@@ -40,8 +40,8 @@ const projects = [
     title: "Code Quiz",
     description:
       "A timed quiz that allowed a boot camp student such as myself to learn the fundamentals of JavaScript and note down my high scores so that I can gauge my progress against my peers.",
-    repositoryLink: "https://github.com/Ahmeds226/Code-Quiz",
     deployedLink: "https://ahmeds226.github.io/Code-Quiz/",
+    repositoryLink: "https://github.com/Ahmeds226/Code-Quiz",
   },
   {
     // Work Day Planner:
@@ -51,8 +51,19 @@ const projects = [
     title: "Work Day Planner",
     description:
       "An application that allows employees with a busy schedule to add important events to their daily planner allowing them to manage their time effectively.",
-    repositoryLink: "https://github.com/Ahmeds226/Work-Day-Planner",
     deployedLink: "https://ahmeds226.github.io/Work-Day-Planner/",
+    repositoryLink: "https://github.com/Ahmeds226/Work-Day-Planner",
+  },
+  {
+    // Weather Dashboard:
+    image:
+      "https://raw.githubusercontent.com/Ahmeds226/Weather-Dashboard/main/assets/images/Screenshot%202022-05-30%20at%2023.22.42.png",
+    alt: "Preview of my Weather Dashboard",
+    title: "Weather Dashboard",
+    description:
+      "A weather dashboard utilising a third-party API that allows people such as travellers to see the weather outlook in multiple cities and plan their trip accordingly.",
+    deployedLink: "https://ahmeds226.github.io/Weather-Dashboard/",
+    repositoryLink: "https://github.com/Ahmeds226/Weather-Dashboard",
   },
 ];
 
