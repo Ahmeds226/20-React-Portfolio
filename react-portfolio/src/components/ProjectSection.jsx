8f25df11e32e20727806869395c2feeb1c74a23f
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 15:26:53 +0100

    Completed project section

--- react-portfolio/src/components/ProjectSection.jsx
+++ react-portfolio/src/components/ProjectSection.jsx
@@ -13,19 +13,48 @@ const ProjectSection = ({
   alt,
   title,
   description,
-  deployedLink,
   repositoryLink,
+  deployedLink,
 }) => {
   // Preview section / Images:
-  <div className="card group max-w-md bg-base-100 shadow-lg rounded-lg mx-auto cursor-pointer">
-    <figure className="h-48 overflow-auto">
-      <img
-        className="w-full h-full object-contain object-top p-5 bg-sky-600 group-hover:bg-red-500"
-        // Preview image:
-        src={image}
-        // text if image does not show:
-        alt={alt}
-      />
-    </figure>
-  </div>;
+  return (
+    <div className="card group max-w-md bg-base-100 shadow-lg rounded-lg mx-auto cursor-pointer">
+      <figure className="h-48 overflow-auto">
+        <img
+          className="w-full h-full object-contain object-top p-5 bg-white group-hover:bg-red"
+          // Preview image:
+          src={image}
+          // text if image does not show:
+          alt={alt}
+        />
+      </figure>
+      {/* Card body: */}
+      {/* Description: */}
+      <div className="card-body bg-darkgray space-y-4 group-hover:bg-white group-hover:text-black">
+        {/* Title: */}
+        <h2 className="card-title underline font-bold">{title}</h2>
+        <p className="text-md">{description}</p>
+        <div className="mx-auto space-x-6">
+          {/* Repository Link and Icon: */}
+          {repositoryLink ? (
+            <a href={repositoryLink} target="_blank" rel="noreferrer">
+              <button className="btn bg-red mx-auto">
+                <IoIosCreate />
+              </button>
+            </a>
+          ) : (
+            ""
+          )}
+          {/* Deployment Link and Icon: */}
+          <a href={deployedLink} target="_blank" rel="noreferrer">
+            <button className="btn bg-red mx-auto">
+              <IoIosDesktop />
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default ProjectSection;
