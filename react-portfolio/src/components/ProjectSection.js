fd52656942b733acc887cb91affad8dec33e944f
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 14:10:53 +0100

    imported desktop icon

--- react-portfolio/src/components/ProjectSection.js
+++ react-portfolio/src/components/ProjectSection.js
@@ -1,3 +1,8 @@
 // Imports:
+
 // IonIcons import:
+// Code // IOS Create icon:
 import { IoIosCreate } from "react-icons/io";
+
+// IOS Desktop Icon:
+import { IoIosDesktop } from "react-icons/io";
