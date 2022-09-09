18a98d16a9a21be1aa76cf06a2756dd8e558b0c6
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 06:58:53 +0100

    Original portfolio added

--- react-portfolio/src/data/projects.js
+++ react-portfolio/src/data/projects.js
@@ -12,22 +12,21 @@ const projects = [
     deployedLink: "https://ahmeds226.github.io/Week-1-Homework/",
   },
   {
-    //
+    // Original Professional Portfolio:
     image:
-      "https://raw.githubusercontent.com/Ahmeds226/Week-1-Homework/master/images/01-html-css-git-homework-demo.png",
-    alt: "picture of Horiseon Code Refactor",
-    title: "Horiseon Code Refactor",
-    description:
-      "An improved codebase, for a marketing agency, that follows accessibility standards to optimise their website for search engines.",
-    repositoryLink: "https://github.com/Ahmeds226/Week-1-Homework",
-    deployedLink: "https://ahmeds226.github.io/Week-1-Homework/",
+      "https://raw.githubusercontent.com/Ahmeds226/Portfolio-Project/main/Images/Preview.png",
+    alt: "Preview of my original portfolio",
+    title: "Original Portfolio",
+    description: "My original portfolio.",
+    repositoryLink: "https://github.com/Ahmeds226/Portfolio-Project",
+    deployedLink: "https://ahmeds226.github.io/Portfolio-Project/",
   },
 ];
 
 export default projects;
 
-// 1. Code Refactor
-// 2. Pro portfolio
+// 1. Code Refactor - Done
+// 2. Pro portfolio - Done
 // 3. Password Generator
 // 4. Code Quiz
 // 5. Day Planner
