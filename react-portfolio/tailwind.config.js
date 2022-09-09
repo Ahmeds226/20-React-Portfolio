ee34cd16708dbaf378cb618cc57ff3e6f94cf702
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 15:10:45 +0100

    Added color theme to tailwind config

--- react-portfolio/tailwind.config.js
+++ react-portfolio/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   // Configured so that I have added the paths to all of my template files:
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      darkgray: "#2B303B",
+      black: "##000000",
+      red: "#EC6055",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
