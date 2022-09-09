6de7d24de10d111db6a7402ac941de6910551617
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-09 14:51:37 +0100

    Created a card group // done preview images

--- react-portfolio/src/components/ProjectSection.jsx
+++ react-portfolio/src/components/ProjectSection.jsx
@@ -4,5 +4,26 @@
 // Code // IOS Create icon:
 import { IoIosCreate } from "react-icons/io";
 
-// IOS Desktop Icon:
+// Deploy // IOS Desktop Icon:
 import { IoIosDesktop } from "react-icons/io";
+
+// Made function to call the project section:
+// Listed the labels that would come up:
+const ProjectSection = ({
+  image,
+  alt,
+  title,
+  description,
+  deployedLink,
+  repositoryLink,
+}) => {
+  <div className="card group max-w-md bg-base-100 shadow-lg rounded-lg mx-auto cursor-pointer">
+    <figure className="h-48 overflow-auto">
+      <img
+        className="w-full h-full object-contain object-top p-5 bg-sky-600 group-hover:bg-red-500"
+        src={image}
+        alt={alt}
+      />
+    </figure>
+  </div>;
+};
